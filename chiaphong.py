0f1641ed22f7e3461fc64c42a751592857a7047e
Mink Alex Vina <64893437+anh-dz@users.noreply.github.com>
2020-12-23 21:05:30 +0700

    Update chiaphong.py

--- chiaphong.py
+++ chiaphong.py
@@ -1,5 +1,5 @@
 import random
-with open('list.txt', 'r', encoding='utf-8') as f:
+with open('list.txt', 'r', encoding='utf-8') as f: #Đọc tên tiếng việt cần encoding = 'utf-8'
 	lis = f.readlines()
 	random.shuffle(lis)
 
@@ -10,7 +10,7 @@ ds = []
 i = 0
 while len(lis)>0:
 	try:
-		ds.append([])
+		ds.append([]) #Thêm 1 phòng
 		for j in range(sohsphong):
 			ds[i].append(lis[0])
 			lis.pop(0)
@@ -19,11 +19,11 @@ while len(lis)>0:
 		break
 
 for i in range(len(ds[sophong])):
-	ds[random.randint(0,sophong-1)].append(ds[sophong][0])
+	ds[random.randint(0,sophong-1)].append(ds[sophong][0]) #random phòng từ 1->số lượng phòng để thêm học sinh vào
 	ds[sophong].pop(0)
 
 for i in range(sophong):
-	f = open(f'phong{i+1}.txt', 'w', encoding = 'utf-8')
+	f = open(f'phong{i+1}.txt', 'w', encoding = 'utf-8') #Tên có thể do các bạn tùy chọn
 	for j in range(len(ds[i])):
 		f.write(ds[i][j])
 	f.close()
